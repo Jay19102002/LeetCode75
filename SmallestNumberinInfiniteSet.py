bd63f626949c2d40b7bc1006a01c0468149ee863
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-03-27 10:08:19 +0530

    𝟮𝟯𝟯𝟲. 𝗦𝗺𝗮𝗹𝗹𝗲𝘀𝘁 𝗡𝘂𝗺𝗯𝗲𝗿 𝗶𝗻 𝗜𝗻𝗳𝗶𝗻𝗶𝘁𝗲 𝗦𝗲𝘁

---
+++ SmallestNumberinInfiniteSet.py
@@ -0,0 +1,37 @@
+# Input
+# ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+# [[], [2], [], [], [], [1], [], [], []]
+# Output
+# [null, null, 1, 2, 3, null, 1, 4, 5]
+
+# Explanation
+# SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+# smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+# smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+# smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+# smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+#                                    // is the smallest number, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.cur = 1
+        self.s =  set()
+
+    def popSmallest(self):
+        if self.s:
+            res = min(self.s)
+            self.s.remove(res)
+            return res
+        else:
+            self.cur += 1
+            return self.cur - 1
+
+    def addBack(self, num):
+        if self.cur > num:
+            self.s.add(num)
+
