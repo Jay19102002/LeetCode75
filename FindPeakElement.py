ebc21ac3a49bd0fbf2d2d58fa359006f2e913410
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-01 18:19:46 +0530

    𝟭𝟲𝟮. 𝗙𝗶𝗻𝗱 𝗣𝗲𝗮𝗸 𝗘𝗹𝗲𝗺𝗲𝗻𝘁

---
+++ FindPeakElement.py
@@ -0,0 +1,22 @@
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: 2
+# Explanation: 3 is a peak element and your function should return the index number 2.
+
+# Example 2:
+# Input: nums = [1,2,1,3,5,6,4]
+# Output: 5
+# Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+def peakElement(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+nums = [1,2,1,3,5,6,4]
+print(peakElement(nums)) 
