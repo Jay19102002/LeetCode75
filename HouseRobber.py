c842081ee3a673b5f50167a6fa68a3e09cfbf066
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-07 18:28:11 +0530

    𝟭𝟵𝟴. 𝗛𝗼𝘂𝘀𝗲 𝗥𝗼𝗯𝗯𝗲𝗿

---
+++ HouseRobber.py
@@ -0,0 +1,21 @@
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+
+def rob(nums):
+    if not nums:
+        return 0
+    n = len(nums)
+    if n == 1:
+        return nums[0]
+    
+    dp = [0] * n
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    for i in range(2, n):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+    return dp[-1]
+
+nums = [1,2,3,1]
+print(rob(nums))  
