a27fff07e1f8cab23b00654ac2cda3509d04379f
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-08 11:48:07 +0530

    𝟳𝟵𝟬. 𝗗𝗼𝗺𝗶𝗻𝗼 𝗮𝗻𝗱 𝗧𝗿𝗼𝗺𝗶𝗻𝗼 𝗧𝗶𝗹𝗶𝗻𝗴

---
+++ DominoandTrominoTiling.py
@@ -0,0 +1,23 @@
+# Input: n = 3
+# Output: 5
+# Explanation: The five different ways are show above.
+
+def numTilings(n):
+        mod = 10**9 + 7
+        if n == 1: return 1
+        if n == 2: return 2
+        if n == 3: return 5
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % mod
+
+        return dp[n]
+
+n = 30
+print(numTilings(n))  
